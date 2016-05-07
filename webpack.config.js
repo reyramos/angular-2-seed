caf5924f3c9efb8110874222510f5da4906d87f1
Rey Ramos <reymundo.ramos@gmail.com>
2016-05-07 00:09:01 -0400

    enhancements

--- webpack.config.js
+++ webpack.config.js
@@ -88,8 +88,6 @@ function makeConfig(options) {
             }),
             new HtmlWebpackPlugin({
                 // Required
-                inject: false,
-                template: 'node_modules/html-webpack-template/index.ejs',
                 template: path.join(appDir, "index.html"),
             }),
             new ReloadPlugin(isDevServer ? 'localhost' : ''),
