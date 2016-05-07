be9bc4e0ccca2a87a5cd1565eb51d187a59ce39f
Rey Ramos <reymundo.ramos@gmail.com>
2016-05-06 23:50:40 -0400

    enhancement using npm run scripts, wip with gruntfile

--- app/ts/vendor.ts
+++ app/ts/vendor.ts
@@ -2,12 +2,9 @@
  * Created by reyra on 5/6/2016.
  */
 // Polyfills
-import 'es6-shim';
-import 'es6-promise';
-import 'es7-reflect-metadata/dist/browser';
-
+import 'es6-shim/es6-shim';
 import 'zone.js/dist/zone';
-import 'zone.js/dist/long-stack-trace-zone';
+import 'reflect-metadata/Reflect';
 
 // Angular 2
 import '@angular/platform-browser';
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-var webpack             = require('webpack'),
-    ReloadPlugin        = require('webpack-reload-plugin'),
-    path                = require('path'),
+var webpack = require('webpack'),
+    ReloadPlugin = require('webpack-reload-plugin'),
+    path = require('path'),
     ChunkManifestPlugin = require('chunk-manifest-webpack-plugin'),
-    HtmlWebpackPlugin   = require('html-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
     WebpackNotifierPlugin = require('webpack-notifier'),
-    ExtractTextPlugin   = require("extract-text-webpack-plugin");
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 /**
  * Support for extra commandline arguments
  */
 var argv = require('optimist')
-    .alias('r','release').default('r', false)
+    .alias('r', 'release').default('r', false)
     .argv;
 
 /**
@@ -19,7 +19,7 @@ var argv = require('optimist')
 var cwd = process.cwd();
 var DEBUG = !argv.release;
 var isDevServer = process.argv.join('').indexOf('webpack-dev-server') > -1;
-var version = require(path.resolve(cwd,'package.json')).version;
+var version = require(path.resolve(cwd, 'package.json')).version;
 var reloadHost = "0.0.0.0";
 var npmRoot = __dirname + "/node_modules";
 var appDir = __dirname + "/app";
@@ -27,7 +27,7 @@ var appDir = __dirname + "/app";
 var entry = ["./app/ts/app.ts"]
 
 if (isDevServer) {
-    entry.unshift("webpack-dev-server/client?http://"+reloadHost+":8080");
+    entry.unshift("webpack-dev-server/client?http://" + reloadHost + ":8080");
 }
 
 function makeConfig(options) {
@@ -69,8 +69,13 @@ function makeConfig(options) {
         plugins: [
             new webpack.IgnorePlugin(/spec\.js$/),
             // new webpack.optimize.CommonsChunkPlugin('core.js'),
-            new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js', minChunks: Infinity }),
-            new webpack.optimize.CommonsChunkPlugin({ name: 'common', filename: 'common.js', minChunks: 2, chunks: ['app', 'vendor'] }),
+            new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity}),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'common',
+                filename: 'common.js',
+                minChunks: 2,
+                chunks: ['app', 'vendor']
+            }),
             // new webpack.optimize.CommonsChunkPlugin({
             //   name: 'angular',
             //   minChunks: Infinity,
@@ -84,7 +89,7 @@ function makeConfig(options) {
             new HtmlWebpackPlugin({
                 template: path.join(appDir, "index.html"),
             }),
-            new ReloadPlugin( isDevServer ? 'localhost' : ''),
+            new ReloadPlugin(isDevServer ? 'localhost' : ''),
             new WebpackNotifierPlugin({
                 title: "ng-book",
                 // contentImage: path.join(appDir, 'images/notifier.png')
@@ -100,7 +105,7 @@ function makeConfig(options) {
             modulesDirectories: [
                 'node_modules', 'app', 'app/ts', '.'
             ],
-            extensions: ["", ".ts", ".js", ".json", ".css"],
+            extensions: ['', '.webpack.js', '.web.js', '.ts', '.js', '.json', '.css', '.less'],
             alias: {
                 // 'rx': '@reactivex/rxjs',
                 'app': 'app',
@@ -113,32 +118,53 @@ function makeConfig(options) {
             ],
 
             loaders: [
-                { test: /\.(png|jpg|gif)$/,   loader: "url-loader?limit=50000&name=[path][name].[ext]" },
-                { test: /\.json$/, loader: 'json' },
-                { test: /^(?!.*\.min\.css$).*\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap")},
-                { test: /\.less$/, loader: "style-loader!css-loader!less-loader"},
-                { test: /\.scss$/, loaders: ['style-loader',
+                {test: /\.(png|jpg|gif)$/, loader: "url-loader?limit=50000&name=[path][name].[ext]"},
+                {test: /\.json$/, loader: 'json'},
+                {
+                    test: /^(?!.*\.min\.css$).*\.css$/,
+                    loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap")
+                },
+                {test: /\.less$/, loader: "style-loader!css-loader!less-loader"},
+                {
+                    test: /\.scss$/, loaders: ['style-loader',
                     ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap"),
                     'sass-loader' +
                     '?outputStyle=expanded&' +
-                    'root='+appDir+'&' +
-                    '&includePaths[]'+npmRoot + '&' +
-                    '&includePaths[]'+appDir
-                ]},
-                { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,         loader: "url-loader" },
-                { test: /\.html$/,    loader: "raw" },
-                { test: /^index\.html$/, loader: "file-loader?name=[path][name].[ext]" },
-                { test: /\.ts$/, loader: 'ts', exclude: [ /test/, /node_modules/]},
-                { test: /vendor\/.*\.(css|js)/, loader: 'file-loader?name=[path][name].[ext]', exclude: [/node_modules/]},
-                { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,    loader: "file-loader?mimetype=application/font-woff&name=[path][name].[ext]" },
-                { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,         loader: "file-loader?mimetype=application/x-font-ttf&name=[path][name].[ext]" },
-                { test: /\.eot(\?v=\d+\.\d+\.\d+)?\??$/,      loader: "file-loader?mimetype=application/vnd.ms-fontobject&name=[path][name].[ext]" },
-                { test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,         loader: "file-loader?mimetype=application/font-otf&name=[path][name].[ext]" },
-                { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,         loader: "url-loader"   },
+                    'root=' + appDir + '&' +
+                    '&includePaths[]' + npmRoot + '&' +
+                    '&includePaths[]' + appDir
+                ]
+                },
+                {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader"},
+                {test: /\.html$/, loader: "raw"},
+                {test: /^index\.html$/, loader: "file-loader?name=[path][name].[ext]"},
+                {
+                    test: /vendor\/.*\.(css|js)/,
+                    loader: 'file-loader?name=[path][name].[ext]',
+                    exclude: [/node_modules/]
+                },
+                {
+                    test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "file-loader?mimetype=application/font-woff&name=[path][name].[ext]"
+                },
+                {
+                    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "file-loader?mimetype=application/x-font-ttf&name=[path][name].[ext]"
+                },
+                {
+                    test: /\.eot(\?v=\d+\.\d+\.\d+)?\??$/,
+                    loader: "file-loader?mimetype=application/vnd.ms-fontobject&name=[path][name].[ext]"
+                },
+                {
+                    test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "file-loader?mimetype=application/font-otf&name=[path][name].[ext]"
+                },
+                {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader"},
+                {test: /\.ts$/, loader: 'ts-loader', exclude: [/test/]}
             ],
             noParse: [
-                /\.min\.js/,
-                /vendor[\/\\].*?\.(js|css)$/
+                // /\.min\.js/,
+                // /vendor[\/\\].*?\.(js|css)$/
             ]
         },
         tslint: {
