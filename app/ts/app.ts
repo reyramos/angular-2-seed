3b78a8a44c4aa2219de4d5c18771b201d9004abc
Rey Ramos <reymundo.ramos@gmail.com>
2016-05-06 22:01:17 -0400

    init

---
+++ app/ts/app.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by reyra on 4/17/2016.
+ */
+
+import {Component} from '@angular/core';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+@Component({
+	selector: 'my-app',
+	template: '<h1>My First Angular 2 App</h1>'
+})
+
+export class AppComponent {
+}
+
+
+bootstrap(AppComponent);
---
+++ app/ts/vendor.ts
@@ -0,0 +1,20 @@
+/**
+ * Created by reyra on 5/6/2016.
+ */
+// Polyfills
+import 'es6-shim';
+import 'es6-promise';
+import 'es7-reflect-metadata/dist/browser';
+
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/router';
+import '@angular/http';
+
+// RxJS
+import 'rxjs';
